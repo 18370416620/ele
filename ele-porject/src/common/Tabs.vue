f5e1113d35b973365350f20689a5b1f4e22b6c2d
xieyahao <873736647@qq.com>
2018-01-16 19:01:58 +0800

    初次上传项目

---
+++ ele-porject/src/common/Tabs.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="tabs">
+        <router-link class="tab" v-for="(tabItem,index) in tabsData" :key="index" :to="tabItem.path">
+            {{tabItem.title}}
+        </router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+    name:'tabs',
+    data(){
+        return{
+            tabsData:[
+                {title:'首页',path:'/home'},
+                {title:'发现',path:'/discover'},
+                {title:'订单',path:'/order'},
+                {title:'我的',path:'/mine'}
+            ]
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
---
+++ ele-porject/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../pages/home/Home.vue'
+import Discover from '../pages/discover/Discover.vue'
+import Order from '../pages/order/Order.vue'
+import Mine from '../pages/mine/Mine.vue'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+      {
+        path:'/home',
+        alias:'/',
+        component:Home
+      },
+      {
+        path:'/discover',
+        component:Discover
+      },
+      {
+        path:'/order',
+        component:Order
+      },
+      {
+        path:'/mine',
+        component:Mine
+      },
+      {
+        path:'**',
+        redirect:'/home'
+      }
+  ]
+})
